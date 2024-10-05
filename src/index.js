7d15b81a311a0e09885be4550ef425500bf85c21
aquiles23 <jjoseaquiless@gmail.com>
2024-10-05 18:47:10 -0300

    create email job to alert of vaccines not taken

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import 'dotenv/config'
 import routes from "./routes/index.js";
 import cron from "node-cron";
-import { checkVaccinationExpiry } from "./services/notification.service.js";
+import { checkVaccinationExpiry, checkVaccinesNotTaken } from "./services/notification.service.js";
 
 const app = express();
 
@@ -15,4 +15,6 @@ routes(app);
 app.listen(3333);
 console.log("servidor iniciou na porta 3333")
 
-cron.schedule("0 0 * * *", checkVaccinationExpiry);
+cron.schedule("0 0 * * *", checkVaccinationExpiry); // diario
+
+cron.schedule("0 0 1 * *", checkVaccinesNotTaken); // mensal
--- src/services/mail.js
+++ src/services/mail.js
@@ -26,10 +26,33 @@ export const sendNotificationEmail = async (email, vaccineName, endDate) => {
   const mailOptions = {
     from: "sipavadm@gmail.com",
     to: email,
-    subject: "Notificação de Expiração de Vacina",
+    subject: "Notificação de reforço de Vacina",
     html: `<p>Olá,</p>
-           <p>A vacina <strong>${vaccineName}</strong> está prestes a expirar em <strong>${new Date(endDate).toLocaleDateString()}</strong>.</p>
+           <p>A vacina <strong>${vaccineName}</strong> tem data de reforço para <strong>${new Date(
+             endDate
+           ).toLocaleDateString()}</strong>.</p>
            <p>Por favor, verifique seu calendário de vacinação.</p>`,
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+    console.log("E-mail de notificação enviado com sucesso!");
+  } catch (error) {
+    console.error("Erro ao enviar e-mail de notificação:", error);
+  }
+};
+
+export const vaccinesNotTakenEmail = async (email, vaccines) => {
+  let htmlText = `<p>Olá, lembrando que ainda não se imunizou com: </p>`;
+  for (let vac of vaccines) {
+    htmlText += `<p> ${vac} </p>`;
+  }
+
+  const mailOptions = {
+    from: "sipavadm@gmail.com",
+    to: email,
+    subject: "Notificação de Expiração de Vacina",
+    html: htmlText,
   };
 
   try {
